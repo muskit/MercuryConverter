afc08d72aa308c4ce5c7685941f45fc9b361a18c
Alex <15199219+muskit@users.noreply.github.com>
2025-09-02 15:00:32 -0700

    shrink build package (--self-contain false)

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 <img height="600" alt="image" src="https://github.com/user-attachments/assets/2a06c9b4-6b37-43eb-a7f5-3b2d4577c736" />
 
-
-
 This is a rewrite of [WacK-Repackager](https://github.com/muskit/WacK-Repackager) to work with the upcoming Saturn project.
+
+**Requires [.NET 9.0+](https://dotnet.microsoft.com/en-us/download) to be installed.**
 
 Head over to [HOWTO](HOWTO.md) for required data preparation.
--- build-and-package.sh
+++ build-and-package.sh
@@ -5,8 +5,8 @@ cd $(dirname $0)
 app_version=MercuryConverter-v$(cat src/Assets/version)
 
 # create builds
-dotnet publish src --runtime linux-x64 -c Release -p:PublishSingleFile=True -o "./builds/$app_version-linux-x64"
-dotnet publish src --runtime win-x64 -c Release -p:PublishSingleFile=True -o "./builds/$app_version-win-x64"
+dotnet publish src --runtime linux-x64 -c Release -p:PublishSingleFile=True --self-contained false -o "./builds/$app_version-linux-x64"
+dotnet publish src --runtime win-x64 -c Release -p:PublishSingleFile=True --self-contained false -o "./builds/$app_version-win-x64"
 
 # package builds
 cd builds
